8f559b3e210acccd95e07ab8610b46f49ef36f09
Aleksei Ezhov <alekcey-ezh@yandex-team.ru>
2022-09-18 18:39:47 +0200

    .

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,18 +1,11 @@
-import { Head } from "$fresh/runtime.ts";
-
 export default function Home() {
   return (
-    <>
-      <Head>
-        <link rel="stylesheet" href="style.css" />
-      </Head>
-      <div
-        class="h-screen w-screen grid content-center pl-28 text-white"
-        style="background: linear-gradient(180deg, #2B2A29 0%, #FFFFFF 100%)"
-      >
-        <h1 class="text-6xl">Portfolio</h1>
-        <h2 class="text-2xl">Web-disigner Ezhova Ulyana</h2>
-      </div>
-    </>
+    <div
+      class="h-screen w-screen grid content-center pl-28 text-white"
+      style="background: linear-gradient(180deg, #2B2A29 0%, #FFFFFF 100%)"
+    >
+      <h1 class="text-6xl">Portfolio</h1>
+      <h2 class="text-2xl">Web-disigner Ezhova Ulyana</h2>
+    </div>
   );
 }
