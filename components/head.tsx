2ca3f3bd5ad6f1350f24ecef09c6cd1aa2245434
Aleksei Ezhov <alekcey-ezh@yandex-team.ru>
2022-09-30 10:29:06 +0200

    .

--- components/head.tsx
+++ components/head.tsx
@@ -20,7 +20,7 @@ export function HeadElement({
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
       /> */}
-      <meta name="viewport" content="width=1280" />
+      <meta name="viewport" content="width=1440" />
 
       <link
         rel="apple-touch-icon"
